c54884339f7cd59120fd7e70fe2001a3f4784a33
SebastianRG16 <89326030+SebastianRG16@users.noreply.github.com>
2023-08-25 15:18:39 -0500

    asgi

--- zaperoco_api/asgi.py
+++ zaperoco_api/asgi.py
@@ -10,13 +10,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from consultas.urls import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zaperoco_api.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": URLRouter(
-        websocket_urlpatterns
+    "websocket": AuthMiddlewareStack( 
+        URLRouter(
+            websocket_urlpatterns
+        )
     ),
 })
